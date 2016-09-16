7e1685e3a80a99b5c30dead34d6e44c57b624d63
Ryan Lynch <ryan.lynch@ligo.org>
2016-09-15 22:28:27 -0400

    Added data and script to pickle the data array

---
+++ txt2array.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pickle
+
+##############################################
+if __name__=='__main__':
+
+	#First parse options
+	from optparse import OptionParser
+
+	parser = OptionParser()
+	
+	parser.add_option("", "--text-file", default=None, type="string", help="Path to textfile containing the data")
+	parser.add_option("", "--outdir", default=None, type="string", help="Directory in which to save pickled dictionary")
+
+	opts, args = parser.parse_args()
+	
+	text_file = opts.text_file
+	outdir = opts.outdir
+	
+	#--------------------------------------------------------------
+	
+	#Load in data
+	array = np.genfromtxt(text_file)
+	
+	#Load in header
+	text_openfile = open(text_file,'rt')
+	text_list = text_openfile.readlines()
+	headers = np.array(text_list[0].split()[1:])
+	
+	#Build dictionary
+	dic = {}
+	dic['headers'] = headers
+	dic['data'] = array
+	
+	#Pickle the dictionary
+	pickle.dump(dic,open('%s/pickled_data_dictionary.pkl'%outdir,'wt'))
+	
+	
