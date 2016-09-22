bde17e888d69233945e030b50c0e6aac4d0cfe2c
Ryan Lynch <ryan.lynch@ligo.org>
2016-09-21 22:15:29 -0400

    Modified Ryan's playground script to include RFs and GMM

--- ryans_playground.py
+++ ryans_playground.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.mixture import GMM
 
 #-----------------------------------------------------------------------
 
@@ -31,7 +32,7 @@ for i in xrange(len(X[0])):
 ######################
 ### Random Forests ###
 ######################
-
+"""
 #Initialize index at which we want to split training and cross-validation data sets
 i_divide = 6000
 
@@ -56,7 +57,44 @@ print "Extra ", rf_extra.score(X_norm[i_divide:],y[i_divide:])
 print "Extra ", rf_extra.oob_score_
 
 #Print parameter importances
+print headers[headers != 'BSN']
 print "Raw ", rf.feature_importances_
 print "Normed ", rf_norm.feature_importances_
 print "Extra ", rf_extra.feature_importances_
+"""
+
+#-----------------------------------------------------------------------
+
+###############################
+### Gaussian Mixture Models ###
+###############################
+
+#Initialize index at which we want to split training and cross-validation data sets
+i_divide = 10000
+
+#Initialize GMM object
+gmm_2 = GMM(n_components=2, covariance_type='full')
+gmm_3 = GMM(n_components=4, covariance_type='full')
+
+#Fit the data
+gmm_2.fit(X_norm[:i_divide])
+gmm_3.fit(X_norm[:i_divide])
+
+#Print means of the fit
+print headers[headers != 'BSN']
+print "2 models ", gmm_2.means_
+print "3 models ", gmm_3.means_
+
+#Print BIC and score
+print "2 models ", gmm_2.bic(X_norm[i_divide:])
+print "3 models ", gmm_3.bic(X_norm[i_divide:])
+
+#Print probability under each Gaussian model
+print "2 models ", gmm_2.predict_proba(X_norm[i_divide:])
+print "3 models ", gmm_3.predict_proba(X_norm[i_divide:])
+
+#Print classifications
+print "2 models ", gmm_2.predict(X_norm[i_divide:])
+print "3 models ", gmm_3.predict(X_norm[i_divide:])
+
 
